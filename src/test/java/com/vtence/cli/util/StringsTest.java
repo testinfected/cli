6525f02c64f47c729fc36d70cd14e427ad1c6018
testinfected <vtence@gmail.com>
2014-05-01 16:25:09 -0400

    Remove unused import

--- src/test/java/com/vtence/cli/util/StringsTest.java
+++ src/test/java/com/vtence/cli/util/StringsTest.java
@@ -2,8 +2,6 @@ package com.vtence.cli.util;
 
 import org.junit.Test;
 
-import java.lang.reflect.InvocationTargetException;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
